625f75995aad567cbb778c3a0f56e86cdd0d7e06
Huzaifa Mahmood <14besehbajwa@seecs.edu.pk>
2020-05-03 15:14:24 +0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -3,14 +3,15 @@ class minecraft {
     ensure => directory,
   }
   file {'/opt/minecraft/minecraft_server.jar':
-    ensure => file,
-    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    ensure  => file,
+    source  => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    before  => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
   file {'/opt/minecraft/eula.txt':
-    ensure => file,
+    ensure  => file,
     content => 'eula=true'
   }
   file {'/etc/systemd/system/minecraft.service':
@@ -18,7 +19,8 @@ class minecraft {
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service { 'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   }
 }
