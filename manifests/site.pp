b372ff8d6e078cbed0d01bf23f23a45536efb35b
Huzaifa Mahmood <14besehbajwa@seecs.edu.pk>
2020-05-03 14:33:56 +0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,9 @@ node default {
 node 'master.puppet.vm' {
   include role::master_server
 }
+node 'minetest.puppet.vm': {
+  include role::minecraft_server
+  }
 node /^web/ { 
   include role::app_server
 }
