75d71f7f2db3b8b33834b8409af82023df8ecbd9
Huzaifa Mahmood <14besehbajwa@seecs.edu.pk>
2020-05-03 12:17:59 +0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDOKc9F3BjD+ApN8G6mEGkQYuspUfnZ6EBCdbL4YfMoLTbzeHCnf9dIuR9ijCb079mmsL1t4SKCiUF2EuVzjEM3iTH77BBc251HE66ooTb/T7X6mlGX0ly+tjP5JKK2otGQEriaSbCOdU2evofafPKGOM9jjtMPxBi0hj87BXrhOunGpBsogQNEYllM9qO9f+cLjmw7cT3Fx+9Co/NiOlEJCmXz9rm8IBDlzsUR8HMDfSLxk2YB1Rq+mRJgOkmadLccoD3I7GzpidHrZndQhW2zNykMnVHbUXEaY3qkrMKSwGEnUiXxNNiCaIc3zW7wSPDgoaWqSIJXJ0vz5MnxyW8h',
 	}  
 }
